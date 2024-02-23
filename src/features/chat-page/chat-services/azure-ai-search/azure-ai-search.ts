aef380d3956bed62281b4a391bc3fa4d8b263b85
Azure Pipeline <azure-pipeline@coso.com>
2024-02-23 15:50:15 +0100

    change quickfix to simpler variation

--- src/features/chat-page/chat-services/azure-ai-search/azure-ai-search.ts
+++ src/features/chat-page/chat-services/azure-ai-search/azure-ai-search.ts
@@ -17,7 +17,6 @@ import {
 
 export interface AzureSearchDocumentIndex {
   id: string;
-  content: string;
   pageContent: string;
   embedding?: number[];
   user: string;
@@ -175,15 +174,12 @@ export const ExtensionSimilaritySearch = async (props: {
           newDocument[key] = document[key];
         }
       }
-      console.log("oldDocument", document);
-      console.log("newDocument", newDocument);
 
       results.push({
         score: result.score,
         document: newDocument, // Use the newDocument object instead of the original document
       });
     }
-    console.log("🟢 Retrieving documents", results);
 
     return {
       status: "OK",
@@ -215,7 +211,6 @@ export const IndexDocuments = async (
         chatThreadId,
         user: await userHashedId(),
         pageContent: doc,
-        content: doc,
         metadata: fileName,
         embedding: [],
       };
