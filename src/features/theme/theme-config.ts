add5ba18c2faa926f2f8b8afea82d106348ab8c4
Azure Pipeline <azure-pipeline@coso.com>
2024-02-29 16:45:00 +0100

    change colors

--- src/features/theme/theme-config.ts
+++ src/features/theme/theme-config.ts
@@ -1,8 +1,8 @@
-export const AI_NAME = "Thomas' Chat Thingy";
-export const AI_DESCRIPTION = "This Chat Thingy is a friendly AI assistant.";
+export const AI_NAME = "Corporate Software Chat";
+export const AI_DESCRIPTION = "This CoSo Chat is a friendly AI assistant.";
 export const CHAT_DEFAULT_PERSONA = AI_NAME + " default";
 
-export const CHAT_DEFAULT_SYSTEM_PROMPT = `You are a friendly ${AI_NAME} AI assistant. You must always return in markdown format.
+export const CHAT_DEFAULT_SYSTEM_PROMPT = `You are the friendly ${AI_NAME} AI assistant. You must always return in markdown format.
 
 You have access to the following functions:
 1. create_img: You must only use the function create_img if the user asks you to create an image.`;
